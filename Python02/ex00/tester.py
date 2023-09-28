552768ca296ca1c5caf2f3b223e66fce2fb5f3ad
laian <tanlaian2004@gmail.com>
2023-09-28 15:22:41 +0800

    fix: load

--- Python02/ex00/tester.py
+++ Python02/ex00/tester.py
@@ -1,3 +1,13 @@
 from load_csv import load
 
 print(load("../life_expectancy_years.csv"))
+
+try:
+    load("../balls.csv")
+except AssertionError as err:
+    print(err.args[0])
+
+try:
+    load("../tester.py")
+except AssertionError as err:
+    print(err.args[0])
