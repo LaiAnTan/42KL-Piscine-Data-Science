46690bd45eeed94cb9daea6e9187f247baba86fa
tlai-an <tlai-an@student.42kl.edu.my>
2023-09-28 14:57:51 +0800

    feat: changes

--- Python02/ex00/load_csv.py
+++ Python02/ex00/load_csv.py
@@ -10,8 +10,9 @@ def load(path: str) -> pd.DataFrame:
     """
     try:
         df = pd.read_csv(path)
-    except Exception as err:
-        print(f"Error: {err.args[0]}")
+    except Exception:
+        print(f"Error: bad file")
+        return
 
     print(f"Loading DataFrame of size {df.shape}")
     return df
