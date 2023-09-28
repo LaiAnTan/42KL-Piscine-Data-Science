bde7b14c543c110cad7ead708188a5e57f96e2ab
tlai-an <tlai-an@student.42kl.edu.my>
2023-09-28 18:56:42 +0800

    feat: more tests

--- Python04/ex00/tester.py
+++ Python04/ex00/tester.py
@@ -12,3 +12,15 @@ ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh",
 print("-----")
 
 ft_statistics(toto="mean", tutu="median", tata="quartile")
+
+print("-----")
+
+ft_statistics(42, 115, 30, 151, 6400, toto="mean", tutu="std",
+              tata="quartile", titi="djdj")
+print("-----")
+ft_statistics(51, 735, 455, 8, 7, 74, 75, hello="median", world="var")
+print("-----")
+ft_statistics(5, 75, 450, 18, 597, 27474, 48575,
+              ejfhhe='fdff', ejdjdejn="demiane")
+print("-----")
+ft_statistics(hello="std", world="median")
--- Python04/ex01/tester.py
+++ Python04/ex01/tester.py
@@ -12,3 +12,25 @@ another_counter = outer(1.5, pow)
 print(another_counter())
 print(another_counter())
 print(another_counter())
+print("---")
+
+
+def cube(x: int | float) -> int | float:
+    """Returns the cube of x"""
+    return (x * x * x)
+
+
+my_counter = outer(6, square)
+print(my_counter())
+print(my_counter())
+print(my_counter())
+print("---")
+another_counter = outer(1.6, pow)
+print(another_counter())
+print(another_counter())
+print(another_counter())
+print("---")
+again_another_counter = outer(4, cube)
+print(again_another_counter())
+print(again_another_counter())
+print(again_another_counter())
